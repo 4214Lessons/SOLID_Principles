b00a4bde5c3ab3f9b4e07104abb79066c2e5975a
Tural Novruzov <novruzov_t@itstep.edu.az>
2022-11-11 17:47:35 +0400

    Add InterfaceSegregationPrinciple

--- InterfaceSegregationPrinciple/Program.cs
+++ InterfaceSegregationPrinciple/Program.cs
@@ -1,9 +1,148 @@
-﻿#region ISP_Before
+﻿namespace ISP_Before
+{
+    interface ICloudProvide
+    {
+        void StoreFile(string name);
+        void GetFile(string name);
+        void CreteServer(string region);
+        void ListServer(string region);
+        void GetCDNAddress();
+    }
 
-#endregion
+
+
+    class Amazon : ICloudProvide
+    {
+        public void StoreFile(string name)
+        {
+            // do smth
+        }
+
+        public void GetFile(string name)
+        {
+            // do smth
+        }
+
+        public void CreteServer(string region)
+        {
+            // do smth
+        }
+
+        public void ListServer(string region)
+        {
+            // do smth
+        }
+
+        public void GetCDNAddress()
+        {
+            // do smth
+        }
+    }
 
 
 
-#region ISP_After
 
-#endregion
+    class Dropbox : ICloudProvide
+    {
+        public void StoreFile(string name)
+        {
+            // do smth
+        }
+
+        public void GetFile(string name)
+        {
+            // do smth
+        }
+
+        public void CreteServer(string region)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ListServer(string region)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetCDNAddress()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+namespace ISP_After
+{
+    interface ICloudHostingProvide
+    {
+        void CreteServer(string region);
+        void ListServer(string region);
+    }
+
+    interface ICDNProvide
+    {
+        void GetCDNAddress();
+    }
+
+    interface ICloudStorageProvide
+    {
+        void StoreFile(string name);
+        void GetFile(string name);
+    }
+
+
+
+
+
+    class Amazon : ICDNProvide, ICloudStorageProvide, ICloudHostingProvide
+    {
+        public void StoreFile(string name)
+        {
+            // do smth
+        }
+
+        public void GetFile(string name)
+        {
+            // do smth
+        }
+
+        public void CreteServer(string region)
+        {
+            // do smth
+        }
+
+        public void ListServer(string region)
+        {
+            // do smth
+        }
+
+        public void GetCDNAddress()
+        {
+            // do smth
+        }
+    }
+
+
+    class Dropbox : ICloudStorageProvide
+    {
+        public void StoreFile(string name)
+        {
+            // do smth
+        }
+        public void GetFile(string name)
+        {
+            // do smth
+        }
+    }
+}
